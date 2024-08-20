84b4362ef87b8375a93ecb7cc127173e1ace6aa7
Felipe Zarco <felipezarco@hotmail.com>
2024-08-19 23:03:41 -0300

    Remove duplicated comment to increase code readability and organization

--- mod.ts
+++ mod.ts
@@ -1,73 +1,22 @@
 /**
+ * `isness`
+ * 
  * A simple library to check if a value is what you expect it to be.
- *
- * ## Usage
- *
- * ### Install
- *
- * Deno:
- *
- * ```sh
- * deno add @zarco/isness
- * ```
- *
- * Node:
- *
- * ```sh
- * npx jsr add @zarco/isness
- * ```
- *
- * ### Usage
- *
- * ```ts
- * import * as is from "@zarco/isness";
- *
- * if (is.string("hello")) {
- *  // do something
- * }
- * ```
- *
+ * 
  * ### Methods
- *
- * The tables below show the methods available in the package, divided by category.
- * Each example consists of one or more values that evaluate to true for the
- * method.
  *
  * #### Character
  *
- * The following methods check if a string is composed of a specific set of
- * characters.
- *
- * | Method         | Description                                                         | Example  |
- * | -------------- | ------------------------------------------------------------------- | -------- |
- * | `alphabetic`   | Checks if a string is alphabetic                                    | "abc"    |
- * | `alphanumeric` | Checks if the given string consists only of alphanumeric characters | "abc123" |
- * | `ascii`        | Checks if a string is ascii                                         | "abc123" |
- * | `digit`        | Checks if a string is a digit                                       | "3"      |
- * | `hexadecimal`  | Checks if a string is hexadecimal                                   | "a1b2c3" |
- * | `lowercase`    | Checks if a string is lowercase                                     | "abc"    |
- * | `uppercase`    | Checks if a string is uppercase                                     | "ABC"    |
- * | `whitespace`   | Checks if a string is a whitespace                                  | " "      |
+ * Check if a string is composed of a specific set of characters.
  *
  * #### Type
  *
- * The following methods check if a value is of a specific type.
- *
- * | Method        | Description                       | Example           |
- * | ------------- | --------------------------------- | ----------------- |
- * | `array`       | Checks if a value is an array     | [1, 2, 3]         |
- * | `string`      | Checks if a value is a string     | "hello"           |
- * | `boolean`     | Checks if a value is a boolean    | true              |
- * | `booleanLike` | Checks if a value is boolean-like | true, "true" or 1 |
- * | `number`      | Checks if a value is a number     | 123, "123", 0, 1.3|
+ * Methods that check if a value is of a specific type.
  *
  * #### Date
  *
- * The following are the date-related methods.
+ * Date-related methods.
  *
- * | Method | Description                 | Example                  |
- * | ------ | --------------------------- | ------------------------ |
- * | `date` | Checks if a value is a date | new Date(), "2021-01-01" |
  */
 
 import { alphabetic } from "./character.ts";
